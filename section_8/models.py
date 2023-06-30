ff2f7b829d3533de59b3135a8535012afaed79d7
shishirsabbir <shishir.sabbir@gmail.com>
2023-06-30 20:40:46 +0600

    to resume

--- section_8/models.py
+++ section_8/models.py
@@ -1,11 +1,8 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer
+
 
 class Todos(Base):
     __tablename__ = 'todos'
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String)
-    description = Column(String)
-    priority = Column(Integer)
-    complete = Column(Boolean, default=False)
