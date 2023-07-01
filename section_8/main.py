80095e5954a0a45bfa19456bcffc39dbbb7b8ed0
shishirsabbir <shishir.sabbir@gmail.com>
2023-07-01 07:37:36 +0600

    database connection setup

---
+++ section_8/main.py
@@ -0,0 +1,23 @@
+import models
+from models import Todos
+from typing import Annotated
+from sqlalchemy.orm import Session
+from fastapi import FastAPI, Depends
+from database import engine, SessionLocal
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get('/')
+async def read_all(db: Annotated[Session, Depends(get_db)]):
+    return db.query(Todos).all() 
